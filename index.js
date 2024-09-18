cda351ad1c5045d1ea88eda7ccd495831ab90736
Sebras22 <sebastien.branly@efrei.net>
2024-09-18 11:12:19 +0200

    maj

--- index.js
+++ index.js
@@ -1,44 +1,36 @@
 import express from "express";
 import cors from "cors";
-import { getCollection } from "./mongodb.js";
+import { getCollection, addTaskToDb } from "./mongodb.js";
 
-import addTask from "./model.js";
-import { ok } from "assert";
 const app = express();
-const port = 5173;
+const port = 3000;
 
-app.use(express.json()); //récupère données du client
+app.use(express.json());
 app.use(cors());
-//tsc pour traduire le ts en js
-
-// var tasks = [findresult];
 
 app.get("/task", async (req, res) => {
     res.json(await getCollection());
-    // res.json(tasks);
 });
 
-app.post("/new", (req, res) => {
-    let name = req.body.name;
-    let description = req.body.description;
+app.post("/new", async (req, res) => {
+    let { name, description } = req.body;
     console.log(name, description);
-    addTask(tasks, name, description);
-    res.json({ message: ok });
+
+    const result = await addTaskToDb(name, description);
+
+    if (result.acknowledged) {
+        res.json({ message: "Tâche ajoutée avec succès !" });
+    } else {
+        res.status(500).json({
+            message: "Erreur lors de l'ajout de la tâche.",
+        });
+    }
 });
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.get("/hello", (req, res) => {
-    res.send("bonjour");
-});
-
-app.get("/json", (req, res) => {
-    let test = { name: "SB" };
-    res.json(test);
-});
-
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`App en écoute sur le port ${port}`);
 });
